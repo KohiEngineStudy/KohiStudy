88dc5b620aff37f55051de6b06e42f98197f73aa
neogeoss1 <neogeo-s@hanmail.net>
2022-12-07 01:00:52 -0500

    Platform_layer_completion_2

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -12,7 +12,7 @@ void report_assertion_failure(const char * expression, const char * message, con
 }
 
 b8 initialize_logging(){
-    return true;
+    return TRUE;
 }
 
 void shutdown_logging(){
@@ -21,9 +21,10 @@ void shutdown_logging(){
 
 void log_output(log_level level, const char * message, ...){
     const char * level_strings[6] = {"[FATAL]: ","[ERROR]: ", "[WARN]: ", "[INFO]: ", "[DEBUG]: ", "[TRACE]: "};
-    // b8 is_error = level < 2;
+    b8 is_error = level < LOG_LEVEL_WARN;
 
-    char out_message[32000];
+    const i32 msg_length = 32000;
+    char out_message[msg_length];
     memset(out_message, 0, sizeof(out_message));
 
     __builtin_va_list arg_ptr;
@@ -37,6 +38,11 @@ void log_output(log_level level, const char * message, ...){
 
     // platform specific output 
 
-    printf("%s", out_message2);
+    // printf("%s", out_message2);
+    if (is_error){
+        platform_console_write_error(out_message2, level);
+    } else {
+        platform_console_write(out_message2, level);
+    }
 
 }
