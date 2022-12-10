dc6154fc846e8472fe668b5124ba3b3aca7004fa
neogeoss1 <neogeo-s@hanmail.net>
2022-12-10 00:07:38 -0500

    application layer commit

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -2,24 +2,22 @@
 #include "core\assert.h"
 
 
-#include "platform\platform.h"
+#include "core\application.h"
 
 int main(void)
 {
-    KFATAL("A test message: %f", 3.14f);
-    KERROR("A test message: %f", 3.14f);
-    KWARN("A test message: %f", 3.14f);
-    KINFO("A test message: %f", 3.14f);
-    KDEBUG("A test message: %f", 3.14f);
-    KTRACE("A test message: %f", 3.14f);
+    
+    application_config config;
+    config.start_pos_x = 100;
+    config.start_pos_y = 100;
+    config.start_width = 1200;
+    config.start_height = 720;
+    config.name = "Kohi Engine Testbed";
 
-    platform_state state;
-    if(platform_startup(&state, "Kohi Engine Testbed", 100, 100, 1200, 720)){
-        while(TRUE){
-            platform_pump_messages(&state);
-        }
-    }
-    platform_shutdown(&state);
+    application_create(&config);
+
+    application_run();
+    
 
     return 0;
 }
